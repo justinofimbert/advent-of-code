789b3f35bdbf4b3ee977be0370543686a7b29ac3
justinofimbert <justinofimbert@gmail.com>
2023-07-30 00:59:31 -0300

    Add day 3 problem 1 solution

---
+++ day_3/a.py
@@ -0,0 +1,43 @@
+from typing import Union
+
+Numeric = Union[int, str]
+
+
+class Santa:
+    def __init__(self, location: Numeric = (0, 0)) -> None:
+        self.location = location
+        self.path = [location]
+
+    def move(self, direction: str) -> None:
+        if direction not in "^>v<":
+            return None
+
+        if direction == "^":
+            self.location = (self.location[0], self.location[1] + 1)
+
+        elif direction == ">":
+            self.location = (self.location[0] + 1, self.location[1])
+
+        elif direction == "v":
+            self.location = (self.location[0], self.location[1] - 1)
+
+        elif direction == "<":
+            self.location = (self.location[0] - 1, self.location[1])
+
+        self.path.append(self.location)
+
+    def path(self) -> list:
+        return self.path
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as file:
+        directions = file.read()
+
+    santa = Santa()
+    for direction in directions:
+        santa.move(direction)
+
+    houses_visited_by_santa = len(set(santa.path))
+
+    print(f"houses visited by santa: {houses_visited_by_santa}")
